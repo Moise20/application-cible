6e49bed827203210a028600052c39710432bfb64
PANA <essognim-moise.pana@soprasteria.com>
2025-07-01 14:59:30 +0200

    initialisation du projet Back avec springboot

---
+++ src/main/java/com/hackathon/application_cible/ApplicationCibleApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.application_cible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationCibleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/application_cible/ApplicationCibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.application_cible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationCibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
